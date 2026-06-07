f02bf2032c720464df4384ad3dced23499b0e7a3
Vaibhav Mulay <mulayvaibhav@gmail.com>
2026-06-07 19:42:21 +0200

    Added PA06 GPIO output

--- src/main.c
+++ src/main.c
@@ -3,40 +3,63 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE    DT_ALIAS(led0)
+#define PA06_NODE    DT_ALIAS(pa06_out)
 
 #if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#error "led0 alias is not defined in the board devicetree"
+#error "led0 alias is not defined"
 #endif
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#if !DT_NODE_HAS_STATUS(PA06_NODE, okay)
+#error "pa06_out alias is not defined"
+#endif
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec pa06 = GPIO_DT_SPEC_GET(PA06_NODE, gpios);
 
 int main(void)
 {
     int ret;
+    bool state = false;
     int counter = 0;
-    bool led_state = false;
 
-    if (!gpio_is_ready_dt(&led)) {
-        printk("LED GPIO device is not ready\n");
+    if (!gpio_is_ready_dt(&led0)) {
+        printk("LED0 GPIO device is not ready\n");
         return 0;
     }
 
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    if (!gpio_is_ready_dt(&pa06)) {
+        printk("PA06 GPIO device is not ready\n");
+        return 0;
+    }
+
+    /*
+     * GPIO_OUTPUT_INACTIVE configures the pin as output.
+     * Since we do NOT use GPIO_OPEN_DRAIN, this is normal push-pull output.
+     */
+    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
     if (ret < 0) {
-        printk("Failed to configure LED GPIO\n");
+        printk("Failed to configure LED0\n");
         return 0;
     }
 
-    printk("My first Zephyr app started on ATSAMV71\n");
+    ret = gpio_pin_configure_dt(&pa06, GPIO_OUTPUT_INACTIVE);
+    if (ret < 0) {
+        printk("Failed to configure PA06\n");
+        return 0;
+    }
+
+    printk("PA06 configured as push-pull GPIO output\n");
 
     while (1) {
-        led_state = !led_state;
-        gpio_pin_set_dt(&led, led_state);
+        state = !state;
 
-        printk("Counter = %d, LED = %s\n",
+        gpio_pin_set_dt(&led0, state);
+        gpio_pin_set_dt(&pa06, state);
+
+        printk("Counter = %d, PA06 = %s\n",
                counter,
-               led_state ? "ON" : "OFF");
+               state ? "HIGH" : "LOW");
 
         counter++;
         k_msleep(1000);
